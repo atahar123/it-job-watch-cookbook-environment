8af542499e5c7abd4fab719809231dc7cd72d75a
Atahar Uddin <AtaharUddin@tcadmins-air-5.home>
2020-04-17 12:03:10 +0100

    done

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -17,13 +17,16 @@ describe 'it-job-watch-cookbook-environment::default' do
     end
   end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'runs apt get update' do
+      expect(chef_run).to update_apt_update 'update_sources'
+    end
+
+    it 'should include recipe poise-python' do
+      expect(chef_run).to install_package 'python3-pip'
+    end
+
+    it 'should create a Downloads folder' do
+      expect(chef_run).to create_directory '/home/ubuntu/Downloads'
     end
   end
-end
